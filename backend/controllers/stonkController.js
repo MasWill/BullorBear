e9c0dec7085cc770791b8fbecfae92998c34b614
Mason Williams <mason@Masons-MacBook-Air.local>
2022-10-22 00:45:02 -0400

    added to controller file

--- backend/controllers/stonkController.js
+++ backend/controllers/stonkController.js
@@ -1,10 +1,19 @@
-const Stonk = require('../models/workoutmodel')
 
 // list of stonks
 const getStonks = async (req,res) => {
-    const stonks = 
-
-    res.status(200).json(workouts)
+    req.get({
+        url: url,
+        json: true,
+        headers: {'User Agent': 'request'}
+    }, (err,res,data) => {
+        if(err) {
+            console.log(err);
+        } else if(res.statusCode != 200) {
+            console.log('Status: ', res.statusCode)
+        } else {
+            console.log(data)
+        }
+    });
 }
 
 module.exports = getStonks
