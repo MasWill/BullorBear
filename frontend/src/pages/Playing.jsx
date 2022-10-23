08ada109ffac29efece35c32df95392894baaf2b
Mason Williams <mason@Masons-MacBook-Air.local>
2022-10-23 05:56:33 -0400

    Before rewrite

--- frontend/src/pages/Playing.jsx
+++ frontend/src/pages/Playing.jsx
@@ -1,24 +1,21 @@
 import { React, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+// API URL
 const URL = "http://localhost:4000/";
 // const pageState = ["main", "playing", "lose"];
 
 const Playing = () => {
   const navigate = useNavigate();
   const [score, setScore] = useState(0);
-  const [left, setLeft] = useState(null); // hold left stock info
-  const [right, setRight] = useState(null); // right stotck info
+  const [left, setLeft] = useState({}); // hold left stock info
+  const [right, setRight] = useState({}); // right stotck info
+  const [isDone, setIsDone] = useState(false); // is done loading
 
-  function startGame() {
-    setScore(0);
-  }
-
-  useEffect(([score, right]) => {
-
+  useEffect(() => {
     // Get a new stock
     const getStonk = async (req, res) => {
-      const response = await fetch(URL + "/stonk");
+      const response = await fetch(URL + "stonk");
       const json = response.json();
       setRight(json);
     };
@@ -27,12 +24,13 @@ const Playing = () => {
     if (score === 0) {
       getStonk();
       setLeft(right);
-      getStonk();  
+      getStonk();
     } else {
       setLeft(right);
       getStonk();
     }
-  });
+    setIsDone(true);
+  }, [score, right]);
 
   function guess(selection) {
     // Incorrect guess
@@ -41,57 +39,60 @@ const Playing = () => {
       (guess === -1 && parseFloat(left["price"]) < parseFloat(right["price"]))
     ) {
       // Redirect to lose screen
-      navigate(`/Lose/{score}`);
+      navigate(`Lose/{score}`);
     } else {
       setScore(score + 1);
     }
   }
 
   return (
-    <div className="playing-content">
-      <h1
-        className="d-flex align-items-center justify-content-center playing-center"
-        style={{ fontSize: "80px" }}
-      >
-        VS
-      </h1>
-      <div className="row">
-        {/* Left */}
-        <div className="playing-bg">
-          <p className="text-light" style={{ fontSize: "32px" }}>
-            {left["tick"]}
-          </p>
-          <p className="text-light" style={{ fontSize: "32px" }}>
-            {left["price"]}
-          </p>
-        </div>
-        {/* Right */}
-        <div className="d-flex playing-bg-2 flex-column">
-          <p className="text-light" style={{ fontSize: "32px" }}>
-          {right["tick"]};
-            
-          </p>
-          <p>
-          {right["price"]};
-          </p>
-          <button
-            className="btn bg-transparent text-black"
-            style={{ fontSize: "32px" }}
-            onClick={() => {
-              guess(1)}}
+    <div>
+      { isDone &&
+        <div className="playing-content">
+          <h1
+            className="d-flex align-items-center justify-content-center playing-center"
+            style={{ fontSize: "80px" }}
           >
-            Higher
-          </button>
-          <button
-            className="btn bg-transparent text-black"
-            style={{ fontSize: "32px" }}
-            onClick={() => {
-                guess(-1)}}
-          >
-            Lower
-          </button>
+            VS
+          </h1>
+          <div className="row">
+            {/* Left */}
+            <div className="playing-bg">
+              <p className="text-light" style={{ fontSize: "32px" }}>
+                {left["tick"]}
+              </p>
+              <p className="text-light" style={{ fontSize: "32px" }}>
+                {left["price"]}
+              </p>
+            </div>
+            {/* Right */}
+            <div className="d-flex playing-bg-2 flex-column">
+              <p className="text-light" style={{ fontSize: "32px" }}>
+                {right["tick"]};
+              </p>
+              <p>{right["price"]};</p>
+              <button
+                className="btn bg-transparent text-black"
+                style={{ fontSize: "32px" }}
+                onClick={() => {
+                  guess(1);
+                }}
+              >
+                Higher
+              </button>
+              <button
+                className="btn bg-transparent text-black"
+                style={{ fontSize: "32px" }}
+                onClick={() => {
+                  guess(-1);
+                }}
+              >
+                Lower
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
+      }
     </div>
   );
 };
