f624865e3ef5294c4746e17db769f243bce8b1ff
jadynLP <jadynlapace@gmail.com>
2022-10-23 02:05:49 -0400

    Deleted unused folder.

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import '../index.css';
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
+    const navigate = useNavigate();
     return (
             <div className="d-flex flex-column align-items-center justify-content-center main-content">
                 <div className="main-title">
@@ -10,7 +12,7 @@ const Main = () => {
                     <h1 className= "title" style={{color:"#FF0000"}}>Bear</h1>
                 </div>
                 <p className="text-light" style={{fontSize:"32px"}}>Which stock is higher in value?</p>
-                <button className="btn bg-transparent text-black" style={{fontSize:"32px", textShadow: "-5px 0 white, 0 5px white, 5px 0 white, 0 -5px white"}}>Play</button>
+                <button className="btn bg-transparent text-black" style={{fontSize:"32px", textShadow: "-5px 0 white, 0 5px white, 5px 0 white, 0 -5px white"}} onClick={() => {navigate('/Playing')}}>Play</button>
             </div>
     )
 }
