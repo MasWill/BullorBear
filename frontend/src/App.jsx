c54c53a47294b18431218a377fac28488a15fd4f
Mason Williams <mason@Masons-MacBook-Air.local>
2022-10-23 02:47:49 -0400

    small imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import Playing from './pages/Playing';
 import Lose from './pages/Lose';
+import { useEffect, useState } from "react"
 
 function App() {
+
+
+
+
   return (
     <div> 
         <BrowserRouter>
