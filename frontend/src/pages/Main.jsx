5dce653f70fdf55e45a5ebf5a2eb3191150b3427
Mason Williams <mason@Masons-MacBook-Air.local>
2022-10-23 02:34:46 -0400

    little changes in main

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-
-
-import '../index.css';
+import '../sytles/index.css';
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
     const navigate = useNavigate();
